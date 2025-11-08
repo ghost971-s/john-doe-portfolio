dbf7eee685e61d849256e9df74cc6573903fa9eb
GHOST <donsmk64@gmail.com>
2025-11-08 22:44:10 +0100

    function finish

---
+++ src/Competence.jsx
@@ -0,0 +1,48 @@
+function Competences () {
+
+return (
+    
+
+
+
+    <div className="col-12 col-md-8 col-lg-6 ">
+    <div className="section-title">
+    <h2>Mes competences</h2>
+    <hr className="my-2 separateur-bleu"/>
+      </div>
+      <div className="progress mb-3">
+        <div className="progress-bar bg-danger" style={{width:"90%"}}>HTML5 90%</div>
+        </div>
+        <div className="progress mb-3">
+<div className="progress-bar bg-info" style={{ width: "80%" }}>
+  CSS3 80%
+</div>
+</div>
+<div className="progress mb-3">
+<div className="progress-bar bg-warning" style={{ width: "70%" }}>
+  JavaScript 70%
+</div> 
+  </div>
+  
+<div className="progress mb-3">
+<div className="progress-bar bg-success" style={{ width: "60%" }}>
+  PHP 60%
+</div>
+</div>
+
+<div className="progress mb-3">
+<div className="progress-bar bg-primary" style={{ width: "50%" }}>
+  React 50%
+</div>
+</div>
+</div>
+
+
+)
+
+}
+
+
+
+
+export default Competences;
